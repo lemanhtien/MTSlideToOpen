4d9b1e08d332ffa92c8d6de23e83fd3ded4fd671
Le Manh Tien <lemanhtien.bka@gmail.com>
2017-10-12 14:54:23 +0700

    Initialize project

---
+++ MTSlideToOpen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MTSlideToOpen
+//
+//  Created by Martin Lee on 10/12/17.
+//  Copyright © 2017 Martin Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
