5da64fc0952346c852075f4c1b2ead8ca9616e14
Le Manh Tien <lemanhtien.bka@gmail.com>
2017-10-16 17:24:13 +0700

    Update examlple, rename source file, add protocol when finishing sliding.

--- MTSlideToOpen/ViewController.swift
+++ MTSlideToOpen/ViewController.swift
@@ -8,19 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    let slideToOpen: MTSlideToOpenControl = {
-        let slide = MTSlideToOpenControl(frame: CGRect(x: 50, y: 100, width: 250, height: 50))
+class ViewController: UIViewController, MTSlideToOpenDelegate {
+    lazy var slideToOpen: MTSlideToOpenView = {
+        let slide = MTSlideToOpenView(frame: CGRect(x: 26, y: 100, width: 317, height: 56))
+        slide.sliderViewTopDistance = 0
+        slide.sliderCornerRadious = 28
+        slide.buttonColor = .red
+        slide.slidingColor = UIColor(red:141.0/255, green:19.0/255, blue:65.0/255, alpha:1.0)
+        slide.delegate = self
+        return slide
+    }()
+    lazy var slideToLock: MTSlideToOpenView = {
+        let slide = MTSlideToOpenView(frame: CGRect(x: 26, y: 200, width: 317, height: 56))
+        slide.sliderViewTopDistance = 0
+        slide.sliderCornerRadious = 28
+        slide.buttonColor = UIColor(red:200.0/255, green:200.0/255, blue:200.0/255, alpha:1.0)
+        slide.slidingColor = UIColor(red:200.0/255, green:200.0/255, blue:200.0/255, alpha:1.0)
+        slide.delegate = self
+        slide.buttonViewLeadingDistance = 20
+        slide.labelText = "Slide To Lock"
+        return slide
+    }()
+    lazy var slideToUnlock: MTSlideToOpenView = {
+        let slide = MTSlideToOpenView(frame: CGRect(x: 26, y: 400, width: 317, height: 56))
+        slide.sliderViewTopDistance = 6
+        slide.sliderCornerRadious = 22
+        slide.delegate = self
+        slide.labelText = "Slide To Unlock"
         return slide
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.addSubview(slideToOpen)
-        slideToOpen.frame = CGRect(x: 26, y: 400, width: 317, height: 56)
-        slideToOpen.sliderTopGap = 6
-        slideToOpen.sliderCornerRadious = 22
-        slideToOpen.canAutomaticResetState = true
+        self.view.addSubview(slideToUnlock)
+        self.view.addSubview(slideToLock)
+    }
+    
+    // MARK: MTSlideToOpenDelegate
+    func mtSlideToOpenDelegateDidFinish(_ sender: MTSlideToOpenView) {
+        let alertController = UIAlertController(title: "", message: "Done!", preferredStyle: .alert)
+        let doneAction = UIAlertAction(title: "Okay", style: .default) { (action) in
+            sender.resetStateWithAnimation(false)
+        }
+        alertController.addAction(doneAction)
+        self.present(alertController, animated: true, completion: nil)
+        
     }
 }
 
--- Source/MTSlideToOpenView.swift
+++ Source/MTSlideToOpenView.swift
@@ -8,7 +8,11 @@
 
 import UIKit
 
-class MTSlideToOpenControl: UIView, UIGestureRecognizerDelegate {
+protocol MTSlideToOpenDelegate {
+    func mtSlideToOpenDelegateDidFinish(_ sender: MTSlideToOpenView)
+}
+
+class MTSlideToOpenView: UIView {
     // MARK: Private Views
     private let textLabel: UILabel = {
        let label = UILabel.init()
@@ -22,16 +26,26 @@ class MTSlideToOpenControl: UIView, UIGestureRecognizerDelegate {
         let view = UIView()
         return view
     }()
+    private let draggedView: UIView = {
+        let view = UIView()
+        return view
+    }()
     private let view: UIView = {
        let view = UIView()
         return view
     }()
     // MARK: Public Properties
+    var delegate: MTSlideToOpenDelegate?
     var animationVelocity: Double = 0.2
-    var sliderTopGap: CGFloat = 8.0 {
+    var sliderViewTopDistance: CGFloat = 8.0 {
         didSet {
-            setStyle()
+            topSliderConstraint?.constant = sliderViewTopDistance
             layoutIfNeeded()
+        }
+    }
+    var buttonViewLeadingDistance: CGFloat = 0.0 {
+        didSet {
+            updateButtonViewLeadingPosition(buttonViewLeadingDistance)
         }
     }
     var sliderCornerRadious: CGFloat = 30.0 {
@@ -40,24 +54,73 @@ class MTSlideToOpenControl: UIView, UIGestureRecognizerDelegate {
             layoutIfNeeded()
         }
     }
-    var sliderBackgroundColor: UIColor = UIColor(red:0.1, green:0.61, blue:0.84, alpha:0.1)
-    var buttonColor: UIColor = UIColor(red:0.1, green:0.61, blue:0.84, alpha:1)
-    var labelText: String = "Swipe to sign document"
-    var labelFont: UIFont = UIFont.systemFont(ofSize: 15.0)
-    var labelTextColor: UIColor = UIColor(red:0.1, green:0.61, blue:0.84, alpha:1)
-    var labelTextAlignment: NSTextAlignment = .center
-    var gapTextLabelLeading: CGFloat = 0
-    var canAutomaticResetState: Bool = false
+    var isEnabled:Bool = true {
+        didSet {
+            setStyle()
+        }
+    }
+    var disableButtonViewColor: UIColor = UIColor(red:182.0/255, green:192.0/255, blue:202.0/255, alpha:1) {
+        didSet {
+            setStyle()
+        }
+    }
+    var disableSliderViewColor: UIColor = UIColor(red:245.0/255, green:247.0/255, blue:250.0/255, alpha:1) {
+        didSet {
+            setStyle()
+        }
+    }
+    var sliderBackgroundColor: UIColor = UIColor(red:0.1, green:0.61, blue:0.84, alpha:0.1) {
+        didSet {
+            setStyle()
+        }
+    }
+    
+    var slidingColor:UIColor = UIColor(red:25.0/255, green:155.0/255, blue:215.0/255, alpha:0.7) {
+        didSet {
+            setStyle()
+        }
+    }
+    
+    var buttonColor: UIColor = UIColor(red:0.1, green:0.61, blue:0.84, alpha:1) {
+        didSet {
+            setStyle()
+        }
+    }
+    
+    var labelText: String = "Swipe to sign document" {
+        didSet {
+            setStyle()
+        }
+    }
+    var labelFont: UIFont = UIFont.systemFont(ofSize: 15.0) {
+        didSet {
+            setStyle()
+        }
+    }
+    var labelTextColor: UIColor = UIColor(red:0.1, green:0.61, blue:0.84, alpha:1) {
+        didSet {
+            setStyle()
+        }
+    }
+    var labelTextAlignment: NSTextAlignment = .center {
+        didSet {
+            setStyle()
+        }
+    }
+    var textLabelLeadingDistance: CGFloat = 0 {
+        didSet {
+            leadingTextLabelConstraint?.constant = textLabelLeadingDistance
+        }
+    }
+    
     // MARK: Private Properties
     private var leadingButtonViewConstraint: NSLayoutConstraint?
     private var leadingTextLabelConstraint: NSLayoutConstraint?
+    private var topSliderConstraint: NSLayoutConstraint?
     private var xPositionInButtonView: CGFloat = 0
     private var xEndingPoint: CGFloat = 0
-    private var isAnimaionEnabled: Bool = true
     private var buttonHeight: CGFloat = 0
-    private var isStopAnimate: Bool = false
-    
-    private var xTapStartLocation: CGFloat = 0
+    private var isFinished: Bool = false
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -66,21 +129,21 @@ class MTSlideToOpenControl: UIView, UIGestureRecognizerDelegate {
     private var panGestureRecognizer: UIPanGestureRecognizer!
     
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)!
     }
     
     private func setupView() {
         self.addSubview(view)
         view.addSubview(buttonView)
         view.addSubview(sliderHolderView)
-        view.addSubview(textLabel)
+        view.addSubview(draggedView)
+        sliderHolderView.addSubview(textLabel)
         view.bringSubview(toFront: self.buttonView)
         setupConstraint()
         setStyle()
         // Add pan gesture
         panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(self.handlePanGesture(_:)))
         panGestureRecognizer.minimumNumberOfTouches = 1
-        panGestureRecognizer.delegate = self
         buttonView.addGestureRecognizer(panGestureRecognizer)
     }
     
@@ -89,6 +152,7 @@ class MTSlideToOpenControl: UIView, UIGestureRecognizerDelegate {
         buttonView.translatesAutoresizingMaskIntoConstraints = false
         sliderHolderView.translatesAutoresizingMaskIntoConstraints = false
         textLabel.translatesAutoresizingMaskIntoConstraints = false
+        draggedView.translatesAutoresizingMaskIntoConstraints = false
         // Setup for view
         view.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
         view.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
@@ -101,28 +165,42 @@ class MTSlideToOpenControl: UIView, UIGestureRecognizerDelegate {
         buttonView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         buttonView.heightAnchor.constraint(equalTo: buttonView.widthAnchor).isActive = true
         // Setup for slider holder view
-        sliderHolderView.topAnchor.constraint(equalTo: view.topAnchor, constant: sliderTopGap).isActive = true
+        topSliderConstraint = sliderHolderView.topAnchor.constraint(equalTo: view.topAnchor, constant: sliderViewTopDistance)
+        topSliderConstraint?.isActive = true
         sliderHolderView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         sliderHolderView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         sliderHolderView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         // Setup for textLabel
-        textLabel.topAnchor.constraint(equalTo: sliderHolderView.topAnchor, constant: sliderTopGap).isActive = true
+        textLabel.topAnchor.constraint(equalTo: sliderHolderView.topAnchor).isActive = true
         textLabel.centerYAnchor.constraint(equalTo: sliderHolderView.centerYAnchor).isActive = true
-        leadingTextLabelConstraint = textLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: gapTextLabelLeading)
+        leadingTextLabelConstraint = textLabel.leadingAnchor.constraint(equalTo: sliderHolderView.leadingAnchor, constant: textLabelLeadingDistance)
         leadingTextLabelConstraint?.isActive = true
         textLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: CGFloat(-8)).isActive = true
+        // Setup for Dragged View
+        draggedView.leadingAnchor.constraint(equalTo: sliderHolderView.leadingAnchor).isActive = true
+        draggedView.topAnchor.constraint(equalTo: sliderHolderView.topAnchor).isActive = true
+        draggedView.bottomAnchor.constraint(equalTo: sliderHolderView.bottomAnchor).isActive = true
+        draggedView.trailingAnchor.constraint(equalTo: buttonView.trailingAnchor).isActive = true
     }
     
     private func setStyle() {
-        buttonView.backgroundColor = buttonColor
+        if isEnabled {
+            buttonView.backgroundColor = buttonColor
+            sliderHolderView.backgroundColor = sliderBackgroundColor
+            textLabel.text = labelText
+        } else {
+            buttonView.backgroundColor = disableButtonViewColor
+            sliderHolderView.backgroundColor = disableSliderViewColor
+            textLabel.text = ""
+        }
         buttonView.layer.masksToBounds = true
         buttonView.layer.cornerRadius = buttonView.frame.width / 2.0
-        textLabel.text = labelText
         textLabel.font = labelFont
         textLabel.textColor = labelTextColor
         textLabel.textAlignment = labelTextAlignment
         sliderHolderView.layer.cornerRadius = sliderCornerRadious
-        sliderHolderView.backgroundColor = sliderBackgroundColor
+        draggedView.backgroundColor = slidingColor
+        draggedView.layer.cornerRadius = sliderCornerRadious
         buttonHeight = buttonView.bounds.height
         xEndingPoint = self.view.frame.maxX - buttonHeight
     }
@@ -149,38 +227,42 @@ class MTSlideToOpenControl: UIView, UIGestureRecognizerDelegate {
     
     // MARK: UIPanGestureRecognizer
     @objc func handlePanGesture(_ sender: UIPanGestureRecognizer) {
+        if isFinished || !isEnabled {
+            return
+        }
         let translatedPoint = sender.translation(in: view).x
-        let xLocation = translatedPoint > 0 ? translatedPoint : (xEndingPoint + translatedPoint)
         switch sender.state {
         case .began:
             break
         case .changed:
-            if xLocation >= xEndingPoint {
+            if translatedPoint >= xEndingPoint {
                 updateButtonViewLeadingPosition(xEndingPoint)
                 return
             }
-            if xLocation <= 0 {
+            if translatedPoint <= buttonViewLeadingDistance {
                 textLabel.alpha = 1
-                updateButtonViewLeadingPosition(0)
+                updateButtonViewLeadingPosition(buttonViewLeadingDistance)
                 return
             }
-            updateButtonViewLeadingPosition(xLocation)
-            textLabel.alpha = (xEndingPoint - xLocation) / xEndingPoint
+            updateButtonViewLeadingPosition(translatedPoint)
+            textLabel.alpha = (xEndingPoint - translatedPoint) / xEndingPoint
             break
         case .ended:
-            if xLocation >= xEndingPoint {
+            if translatedPoint >= xEndingPoint {
                 textLabel.alpha = 0
                 updateButtonViewLeadingPosition(xEndingPoint)
-                isStopAnimate = !canAutomaticResetState
+                // Finish action
+                isFinished = true
+                delegate?.mtSlideToOpenDelegateDidFinish(self)
                 return
             }
-            if xLocation <= 0 {
+            if translatedPoint <= buttonViewLeadingDistance {
                 textLabel.alpha = 1
-                updateButtonViewLeadingPosition(0)
+                updateButtonViewLeadingPosition(buttonViewLeadingDistance)
                 return
             }
             UIView.animate(withDuration: animationVelocity) {
-                self.leadingButtonViewConstraint?.constant = 0
+                self.leadingButtonViewConstraint?.constant = self.buttonViewLeadingDistance
                 self.textLabel.alpha = 1
                 self.layoutIfNeeded()
             }
@@ -190,20 +272,21 @@ class MTSlideToOpenControl: UIView, UIGestureRecognizerDelegate {
         }
     }
     
-    func resetState() {
-        UIView.animate(withDuration: animationVelocity) {
+    // Others
+    func resetStateWithAnimation(_ animated: Bool) {
+        let action = {
             self.leadingButtonViewConstraint?.constant = 0
             self.textLabel.alpha = 1
             self.layoutIfNeeded()
+            //
+            self.isFinished = false
+        }
+        if animated {
+            UIView.animate(withDuration: animationVelocity) {
+               action()
+            }
+        } else {
+            action()
         }
     }
-    
-    // MARK: UIGestureDelegate
-//    override func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
-//        let result = gestureRecognizer.location(in: view).x < xEndingPoint
-//        if result {
-//
-//        }
-//        return result
-//    }
 }
